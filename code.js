e454b65d709e9cc1f9dcbcd2bc82425f1b2059f0
Daniel Söderberg <mr.daniel.soderberg@gmail.com>
2017-11-22 10:33:41 +0100

    jquery

--- code.js
+++ code.js
@@ -1,14 +1,58 @@
+// function renderMovie(data){
+//     document.querySelector(".infosection h1").textContent = data.title;
+//     document.querySelector(".infosection p").textContent = data.review;
+//     document.querySelector(".poster").setAttribute("src", data.imgUrl);
+
+//     let actorList = "";
+//     for (let i = 0; i < data.actors.length; i++) {
+//         actorList += "<li>" + data.actors[i] + "</li>";
+//     }
+
+//     document.querySelector(".infosection ul").innerHTML = actorList;
+// }
+
+// function changeStarRating(rating){
+//   for(let i=1; i<=5; i++){
+//     let star = document.getElementById("star" + i);
+//     if (i <= rating){
+//       star.classList.add("filled");
+//     } else {
+//       star.classList.remove("filled");
+//     }
+//   }
+// }
+
+// for(let i=1; i<=5; i++){
+//   let star = document.getElementById("star" + i);
+//   star.addEventListener("click", function(){
+//     changeStarRating(i);
+//   });
+// }
+
+// renderMovie(movieData);
+
 function renderMovie(data){
-    document.querySelector(".infosection h1").textContent = data.title;
-    document.querySelector(".infosection p").textContent = data.description;
-    document.querySelector(".myImage").setAttribute("src",data.movieImage);
-
-    let actorList = "";
-    for (let i = 0; i < data.listOfActors.length; i++) {
-        actorList += "<li>" + data.listOfActors[i] + "</li>";
+    $(".infosection h1").text(data.title);
+    $(".infosection p").text(data.review);
+    $(".poster").attr("src", data.imgUrl);
+  
+    $(".infosection ul").empty();
+    for(let i=0; i<data.actors.length; i++){
+      $(".infosection ul").append("<li>" + data.actors[i] + "</li>");
     }
-    document.querySelector(".infosection ul").innerHTML = actorList;
-}
-
-renderMovie(movieData);
-
+  }
+  
+  function changeStarRating(rating){
+    $(".filled").removeClass("filled");
+    for(let i=1; i<=rating; i++){
+      $("[data-rating-id=" + i + "]").addClass("filled");
+    }
+  }
+  
+  $(".stars").on("click", "span", function(e){
+    let star = $(e.target);
+    let rating = parseInt(star.attr("data-rating-id"));
+    changeStarRating(rating);
+  });
+  
+  renderMovie(movieData);
--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 let movieData = {
-    titel : "Barry Lyndon",
-    movieImage : "http://www.indiewire.com/wp-content/uploads/2016/06/barry-lyndon-landscape.jpg",
-    description : "How does an Irish lad without prospects become part of 18th-century English nobility?",
-    listOfActors : [
+    title : "Barry Lyndon",
+    imgUrl : "http://www.indiewire.com/wp-content/uploads/2016/06/barry-lyndon-landscape.jpg",
+    review : "How does an Irish lad without prospects become part of 18th-century English nobility?",
+    actors : [
         "Ryan O'Neal", 
         "Marisa Berenson",
         "Patrick Magee"
-    ],
+    ]
 };
 
